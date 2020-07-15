da1b9f3e0701858f63847bbd7e7154cf64186100
Whatley, Cesar <julceswhat@gmail.com>
2020-07-15 19:20:09 -0400

    Created helper components to render main page

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,13 +1,85 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { getMetricMetaInfo } from '../utils/helpers';
+import UdaciSlider from './UdaciSlider';
+import UdaciStepper from './UdaciStepper';
+import DateHeader from './DateHeader';
 
 
 export default class AddEntry extends Component {
+    state = {
+        run: 0,
+        bike: 0,
+        swim: 0,
+        sleep: 0,
+        eat: 0
+    }
+
+    increment = (metric) => {
+        const { max, step } = getMetricMetaInfo(metric);
+
+        this.setState((state) => {
+            const count = state[metric] + step;
+
+            return {
+                ...state,
+                [metric]: count > max ? max : count
+            };
+        });
+    }
+
+    slide = (metric, value) => {
+        this.setState(() => ({
+            [metric]: value
+        }));
+    }
+
+    decrement = (metric) => {
+        const { step } = getMetricMetaInfo(metric);
+
+        this.setState((state) => {
+            const count = state[metric] - step;
+
+            return {
+                ...state,
+                [metric]: count > 0 ? count : 0
+            };
+        });
+    }
 
     render() {
+        const metaInfo = getMetricMetaInfo();
+
         return (
             <View>
+                <DateHeader date={(new Date().toLocaleDateString())} />
+                {
+                    Object.keys(metaInfo).map((key) => {
+                        const { getIcon, type, ...rest } = metaInfo[key];
+                        const value = this.state[key];
+
+                        return (
+                            <View key={key}>
+                                {getIcon()}
+                                {
+                                    type === 'slider'
+                                        ? (
+                                            <UdaciSlider
+                                                value={value}
+                                                onChange={(value) => (this.slide(value))}
+                                                {...rest} />
+                                        ) : (
+                                            <UdaciStepper
+                                                value={value}
+                                                onIncrement={() => this.increment(key)}
+                                                onDecrement={() => this.decrement(key)}
+                                                {...rest} />
+                                        )
+                                }
+                            </View>
+                        )
+                    })
+                }
                 {getMetricMetaInfo('bike').getIcon()}
             </View>
         )
